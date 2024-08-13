a8d11902b2dbdaea462dc0b79320a8473a23cd5f
FarahNabeel21 <nabeelfarah687@gmail.com>
2024-08-13 12:56:18 +0300

    task26

---
+++ Tasks/task27.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Image,View,StyleSheet} from 'react-native';
+const Task27=()=>
+{
+
+
+<View>
+
+
+
+</View>
+
+
+
+
+}
+const styles = StyleSheet.create({
+    container: {
+      paddingTop: 50,
+    },
+    tinyLogo: {
+      width: 50,
+      height: 50,
+    },
+    logo: {
+      width: 66,
+      height: 58,
+    },
+  });
+export default Task27;
