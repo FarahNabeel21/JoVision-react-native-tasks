d91494b9c5f122b2f8fc08451c76a845635177bd
FarahNabeel21 <nabeelfarah687@gmail.com>
2024-08-21 12:16:24 +0300

    Task_27

--- App.tsx
+++ App.tsx
@@ -25,14 +25,16 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 //import Task16 from './Tasks/task16';
-//import Task19 from './Tasks/task19';
+import Task18 from './Tasks/task18';
 //import Task20 from './Tasks/task20';
-//import App from './Tasks/task21'
+import Task21 from './Tasks/task21'
 //import Task22 from './Tasks/task22';
-//import Task23 from './Tasks/task23';
+import Task23 from './Tasks/task23';
 //import Task24 from './Tasks/task24'
 //import Task25 from "./Tasks/task25";
-import Task26 from "./Tasks/task26";
+//import Task26 from "./Tasks/task26";
+//import Task27 from "./Tasks/task27";
+import Task from "./Tasks/tasktrain";
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
@@ -73,15 +75,17 @@ function App(): React.JSX.Element {
   return (
        // <Task16></Task16>
        // <Task17></Task17>
-    //<Task18></Task18>
+   // <Task18></Task18>
     //<Task19></Task19>
    //<Task20></Task20>
-  //<Task21/>
+//  <Task21/>
 //<Task22></Task22>
 //<Task23></Task23>
 //<Task24/>
 //<Task25/>
-<Task26/>
+//<Task26/>
+//<Task27></Task27>
+<Task></Task>
 );
 }
 
